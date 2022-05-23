bdc145906df3ffd30607d8e710c2258f704da36e
SergeySidorchuk <serg_ss@ukr.net>
2022-05-23 15:38:08 +0300

    added db.json, PizzaBclock.jsx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+  //   fetch('http://localhost:3000/db.json')
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setPizzas(json.pizzas);
+  //     });
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-function Button({onClick,className,outline,children}) {
-    return (
-        <button
-            onClick={onClick}
-            className={classNames('button',className, {
-            'button--outline': outline,
-            })}>
-            {children}
-        </button>
-    )
+function Button({ onClick, className, outline, children }) {
+  return (
+    <button
+      onClick={onClick}
+      className={classNames('button', className, {
+        'button--outline': outline,
+      })}>
+      {children}
+    </button>
+  );
 }
 
-
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
 
 export default Button;
