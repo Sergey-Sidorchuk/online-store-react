be02585078df31c0b05e45159f3fe303df02391d
SergeySidorchuk <serg_ss@ukr.net>
2022-05-26 17:33:08 +0300

    added actions, reducers(pizzas,filters)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
 import React from 'react';
 import axios from 'axios';
+import { connect } from 'react-redux';
+// import store from './redux/store';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
+import { setPizzas as setPizzasAction } from './redux/actions/pizzas';
 
-function App() {
-  const [pizzas, setPizzas] = React.useState([]);
+// function App() {
+//   React.useEffect(() => {
+//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
+//       setPizzas(data.pizzas);
+//     });
+//   }, []);
+//   //   fetch('http://localhost:3000/db.json')
+//   //     .then((res) => res.json())
+//   //     .then((json) => {
+//   //       setPizzas(json.pizzas);
+//   //     });
+//   return (
+//     <div className="wrapper">
+//       <Header />
+//       <div className="content">
+//         <Route path="/" render={() => <Home items={pizzas} />} exact />
+//         <Route path="/cart" component={Cart} exact />
+//       </div>
+//     </div>
+//   );
+// }
 
-  React.useEffect(() => {
+class App extends React.Component {
+  componentDidMount() {
     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      setPizzas(data.pizzas);
+      this.props.setPizzasAction(data.pizzas);
     });
-  }, []);
-  //   fetch('http://localhost:3000/db.json')
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setPizzas(json.pizzas);
-  //     });
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Route path="/" render={() => <Home items={pizzas} />} exact />
-        <Route path="/cart" component={Cart} exact />
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />{' '}
+        <div className="content">
+          <Route
+            path="/"
+            render={() => <Home items={this.props.items} />}
+            exact
+          />
+          <Route path="/cart" component={Cart} exact />{' '}
+        </div>{' '}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    items: state.pizzas.items,
+    filters: state.filters,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setPizzas: (items) => dispatch(setPizzasAction(items)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
